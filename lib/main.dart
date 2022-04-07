022b10edc0947faeee861548d651979be4cc96bf
Ashish Kumar <ak2917065@gmail.com>
2022-04-08 00:07:34 +0530

    auto-scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:alumni_app/provider/chat_provider.dart';
 import 'package:alumni_app/provider/current_user_provider.dart';
 import 'package:alumni_app/provider/feed_provider.dart';
+import 'package:alumni_app/provider/people_provider.dart';
 import 'package:alumni_app/screen/home.dart';
 import 'package:alumni_app/screen/wrapper.dart';
 import 'package:alumni_app/services/media_query.dart';
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => CurrentUserProvider()),
         ChangeNotifierProvider(create: (context) => FeedProvider()),
         ChangeNotifierProvider(create: (context) => ChatProvider()),
+        ChangeNotifierProvider(create: (context) => PeopleProvider()),
       ],
       child: MaterialApp(
         // this is needed to provide providers to places where there is no build context
