6e6b0855eab00625fccc12482189dddad670c11b
Charith Bhat <charsept04@gmail.com>
2022-06-23 09:18:42 +0530

    followers and following feature still needs query to be written

--- lib/screen/all_following.dart
+++ lib/screen/all_following.dart
@@ -1,3 +1,4 @@
+import 'package:alumni_app/screen/home.dart';
 import 'package:alumni_app/services/media_query.dart';
 import 'package:flutter/material.dart';
 import 'package:paginate_firestore/paginate_firestore.dart';
@@ -50,73 +51,57 @@ class AllFollowing extends StatelessWidget {
           ),
         ),
       ),
-      // body: SizedBox(
-      //     height: SizeData.screenHeight,
-      //     child: Column(
-      //       children: [
-      //         Expanded(
-      //           child: PaginateFirestore(
-      //             itemsPerPage: 12,
-      //             scrollController: followingScroller,
-      //             itemBuilder: (context, documentSnapshots, index) {
-      //               final data = documentSnapshots[index].data() as Map?;
-      //               NotificationModel notification = NotificationModel.fromJson(
-      //                   documentSnapshots[index].data()
-      //                       as Map<String, dynamic>);
-      //               // log(data.toString());
-      //               final inSec = DateTime.now()
-      //                   .difference(notification.updatedAt)
-      //                   .inSeconds;
-      //               final inMin = DateTime.now()
-      //                   .difference(notification.updatedAt)
-      //                   .inMinutes;
-      //               final inHour = DateTime.now()
-      //                   .difference(notification.updatedAt)
-      //                   .inHours;
-      //               final inDays = DateTime.now()
-      //                   .difference(notification.updatedAt)
-      //                   .inDays;
-      //               // return getPostList(documentSnapshots, index);
-
-      //               return NotificationTile(
-      //                   inDays: inDays,
-      //                   inHour: inHour,
-      //                   inMin: inMin,
-      //                   inSec: inSec,
-      //                   snapshot: documentSnapshots,
-      //                   index: index);
+      body: SizedBox(
+          height: SizeData.screenHeight,
+          child: Column(
+            children: [
+              Expanded(
+                child: PaginateFirestore(
+                  itemsPerPage: 12,
+                  scrollController: followingScroller,
+                  itemBuilder: (context, documentSnapshots, index) {
+                    final data = documentSnapshots[index].data() as Map?;
+                    return Container(child: Text('hello'),);
                     
-      //             },
-      //             query: notificationCollection
-      //                 .where("sentTo", arrayContains: currentUser!.id)
-      //                 .orderBy("updated_at", descending: true),
-      //             itemBuilderType: PaginateBuilderType.listView,
-      //             isLive: true,
-      //             onEmpty: Padding(
-      //               padding: const EdgeInsets.all(20.0),
-      //               child: Center(
-      //                 child: Column(
-      //                   crossAxisAlignment: CrossAxisAlignment.center,
-      //                   mainAxisAlignment: MainAxisAlignment.center,
-      //                   children: [
-      //                     Text(
-      //                       'No Notifcations Yet.',
-      //                       style: Theme.of(context).textTheme.bodyText1,
-      //                     ),
-      //                     const SizedBox(height: 40),
-      //                     Text(
-      //                       "You get notified if someone likes/comments on your posts",
-      //                       style: Theme.of(context).textTheme.bodyText1,
-      //                     ),
-      //                   ],
-      //                 ),
-      //               ),
-      //             ),
-      //           ),
-      //         )
-      //       ],
-      //     ),
-      //   ),
+                  },
+                  // query: notificationCollection
+                  //     .where("sentTo", arrayContains: currentUser!.id)
+                  //     .orderBy("updated_at", descending: true),
+                  query: userCollection.where("owner_id",
+                              whereIn: (currentUser == null)
+                                  ? null
+                                  : currentUser!.following.isEmpty
+                                      ? [currentUser!.id]
+                                      : currentUser!.following +
+                                          [currentUser!.id])
+                          .orderBy("updated_at", descending: true),
+                  itemBuilderType: PaginateBuilderType.listView,
+                  isLive: true,
+                  onEmpty: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'No Notifcations Yet.',
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                          const SizedBox(height: 40),
+                          Text(
+                            "You get notified if someone likes/comments on your posts",
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
     );
   }
 }
